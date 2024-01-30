0ef331cecc9d62040614d03e03dafd770d25de69
PpMaster37 <enderdave37@gmail.com>
2024-01-29 22:55:51 -0800

    pp

--- src/AnimalsInheritance/Tester.java
+++ src/AnimalsInheritance/Tester.java
@@ -3,11 +3,18 @@ package AnimalsInheritance;
 public class Tester {
     public static void main(String[] args){
         Fox pet = new Fox("fox", 4);
+        Mammal petTwo = new Fox("fox",4);
+        Animal petThree = new Fox("fox",4);
         pet.displayInfo();
         pet.giveName("Zesty EagleClaw 100");
         pet.walk();
         pet.action();
         pet.makeSound();
-        System.out.println(pet.toString());
+        System.out.println(pet);
+        petTwo.displayInfo();
+        petTwo.makeSound();
+        petTwo.walk();
+        petThree.makeSound();
+        petThree.displayInfo();
     }
 }
